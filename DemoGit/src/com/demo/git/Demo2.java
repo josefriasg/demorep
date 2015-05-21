5974efdaacc973393e5937115eeb9011724bbb6e
freddycucho <freddycucho@gmail.com>
2015-05-21 12:04:49 -0400

    this is demo of git

---
+++ DemoGit/src/com/demo/git/Demo2.java
@@ -0,0 +1,17 @@
+package com.demo.git;
+
+
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		System.out.println("call new ....");
+	}
+	
+	public Demo2() {
+		
+		System.out.println("demo 2");
+
+	}
+
+}
