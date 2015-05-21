43d43fda77ca9d1ede617a0e807923a53e86f355
sysadmin <sysadmin@10.1.4.215>
2015-05-21 00:24:28 -0500

    nuevo metodo, y cambio de system out por system err y nuevo mensaje AR 93973

--- DemoGit/src/com/demo/git/Comunicacion.java
+++ DemoGit/src/com/demo/git/Comunicacion.java
@@ -3,7 +3,11 @@ package com.demo.git;
 public class Comunicacion {
 
 	public static void main(String[] args) {
-		System.out.println("nuevo archivo de comunicacion");
+		System.err.println("nuevo archivo de comunicacion de algo");
+	}
+	
+	private void enviar(){
+		System.out.println("Envio de documento");
 	}
 	
 }
