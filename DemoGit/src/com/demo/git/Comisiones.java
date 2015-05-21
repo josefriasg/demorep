9484922a45f2530c43d984a85ad4ca2ae3fa9e36
sysadmin <sysadmin@10.1.4.215>
2015-05-21 00:15:08 -0500

    AR 67854 - nuevo archivo de comunicaciones

--- DemoGit/src/com/demo/git/Comisiones.java
+++ DemoGit/src/com/demo/git/Comisiones.java
@@ -9,6 +9,7 @@ public class Comisiones {
 	public static void main(String[] args) {
 		System.out.println("Este metodo es para el calculo de comisiones");
 		System.gc();
+		System.out.println("parte faltante de comisiones");
 	}
 	
 }
---
+++ DemoGit/src/com/demo/git/Comunicacion.java
@@ -0,0 +1,9 @@
+package com.demo.git;
+
+public class Comunicacion {
+
+	public static void main(String[] args) {
+		System.out.println("nuevo archivo de comunicacion");
+	}
+	
+}
