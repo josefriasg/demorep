a63d0a3caacf480d2750d8381389ec8e70855e0f
freddycucho <freddycucho@gmail.com>
2015-05-21 02:03:05 -0400

    HotFix AR 65897

---
+++ DemoGit/src/com/demo/git/Comisiones.java
@@ -0,0 +1,14 @@
+package com.demo.git;
+
+public class Comisiones {
+
+	public Comisiones() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Este metodo es para el calculo de comisiones");
+		System.gc();
+	}
+	
+}
