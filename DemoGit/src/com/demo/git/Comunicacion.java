5dbfc206716bf1e99cceb3dc53bc9bba4cc1c4f6
freddycucho <freddycucho@gmail.com>
2015-05-21 02:18:37 -0400

    Hotfix AR 87324

--- DemoGit/src/com/demo/git/Comunicacion.java
+++ DemoGit/src/com/demo/git/Comunicacion.java
@@ -4,6 +4,9 @@ public class Comunicacion {
 
 	public static void main(String[] args) {
 		System.out.println("nuevo archivo de comunicacion");
+		if(true) {
+			System.out.println("Siempre imprime hasta llenar el heap");
+		}
 	}
 	
 }
